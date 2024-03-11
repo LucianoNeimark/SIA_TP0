c895f58060cd43a7cd34e2d903ab2e112fe06824
Luciano <lucianoneimark@gmail.com>
2024-03-11 12:14:09 -0300

    Printing snorlax graph for 100 throws

--- main.py
+++ main.py
@@ -6,5 +6,4 @@ if __name__ == "__main__":
     factory = PokemonFactory("pokemon.json")
     snorlax = factory.create("snorlax", 100, StatusEffect.NONE, 1)
     print("No noise: ", attempt_catch(snorlax, "heavyball"))
-    for _ in range(10):
-        print("Noisy: ", attempt_catch(snorlax, "heavyball", 0.15))
+
